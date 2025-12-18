ca16bbf60305f863f4c8fba57aabacd616bca59b
wanting1514-netizen <wanting1514@gmail.com>
2025-12-18 10:48:23 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
   export default defineConfig(({ mode }) => {
       const env = loadEnv(mode, '.', '');
       return {
+        base: '/outdoor-ref/',
         server: {
           port: 3000,
           host: '0.0.0.0',
